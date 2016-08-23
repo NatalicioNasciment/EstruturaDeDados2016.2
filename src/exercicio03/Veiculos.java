1e4c946240b8fde070bf0dde17ac437b3a16fbce
NatalicioNasciment <natalicio.eng.soft@gmail.com>
2016-08-22 23:40:57 -0300

    Exercicio 03

--- src/exercicio03/Veiculos.java
+++ src/exercicio03/Veiculos.java
@@ -5,69 +5,79 @@ carga em zonas urbanas. Estes veículos tem capacidade limitada, limite de tempo
 de operação, número de visitas que é capaz de realizar em um dia e atributos
 como: marca, modelo, ano, etc.
  * */
-
 public class Veiculos {
-	//a unidade de medida adotada é KG
-	private static final int CAPACIDADELIMITE = 20000;
-	//a unidade de tempo é a HORA. 
-	private static final int LIMITETEMPO = 1;
+	private static final int CAPACIDADELIMITE = 20000;// a unidade de medida adotada é KG
+	private static final int LIMITETEMPO = 1;// a unidade de tempo é a HORA.
 	private String marca;
 	private String modelo;
 	private int ano;
 	private float capacidade;
 	private int tempo;
 	private int numVisitas;
-	
-	public Veiculos(){}
-	public Veiculos(String marca, String modelo, int ano){
+
+	public Veiculos() {
+	}
+
+	public Veiculos(String marca, String modelo, int ano) {
 		this.setMarca(marca);
 		this.setModelo(modelo);
-		this.setAno(ano);		
+		this.setAno(ano);
 	}
-	public Veiculos(float capacidade, int limiteTempo, int numVisitas){
+
+	public Veiculos(float capacidade, int limiteTempo, int numVisitas) {
 		this.setCapacidade(capacidade);
 		this.setTempo(limiteTempo);
 		this.setNumVisitas(numVisitas);
 	}
-	
+
 	public String getMarca() {
 		return marca;
 	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
 	public String getModelo() {
 		return modelo;
 	}
+
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
+
 	public int getAno() {
 		return ano;
 	}
+
 	public void setAno(int ano) {
 		this.ano = ano;
 	}
+
 	public float getCapacidade() {
-		
 		if (capacidade <= CAPACIDADELIMITE) {
 			return capacidade;
-		}else{
+		} else {
 			return -1;
 		}
 	}
+
 	public void setCapacidade(float capacidade) {
 		this.capacidade = capacidade;
 	}
+
 	public int getTempo() {
 		return tempo;
 	}
+
 	public void setTempo(int tempo) {
 		this.tempo = tempo;
 	}
+
 	public int getNumVisitas() {
 		return numVisitas;
 	}
+
 	public void setNumVisitas(int numVisitas) {
 		this.numVisitas = numVisitas;
 	}
