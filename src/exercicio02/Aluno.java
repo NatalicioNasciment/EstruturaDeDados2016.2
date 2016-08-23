60458da36e1b1ae92ee6342f91e57d8ad9eaafeb
NatalicioNasciment <natalicio.eng.soft@gmail.com>
2016-08-23 00:46:17 -0300

    Exercicio02 (só comecei)

---
+++ src/exercicio02/Aluno.java
@@ -0,0 +1,29 @@
+package exercicio02;
+/*
+ * Considerando as declarações abaixo para representar o cadastro de grupo de
+alunos de diferentes turmas, implemente funções para criar e imprimir o cadastro.
+struct aluno {
+char nome[81];
+char matricula[8];
+char turma;
+float p1;
+float p2;
+float p3;
+}; typedef struct aluno Aluno;
+● Implemente como no exercício anterior
+● Implemente uma função que tenha como valor de retorno a média final obtida
+pelos os alunos de uma determinada turma. A nota final de cada aluno é dada
+pela média das três provas.
+float media_turma (int n, Aluno** turmas, char turma);
+ * */
+public class Aluno {
+	private String nome;
+	private int matricula;
+	private Turma turma;
+	private float p1;
+	private float p2;
+	private float p3;
+	
+	
+
+}
---
+++ src/exercicio02/Turma.java
@@ -0,0 +1,14 @@
+package exercicio02;
+
+public class Turma {
+	private String turma;
+	private Aluno aluno[];
+	
+	public void setTurma(String turma){
+		this.turma = turma;
+	}
+	public String getTurma(){
+		return turma;
+	}
+	
+}
