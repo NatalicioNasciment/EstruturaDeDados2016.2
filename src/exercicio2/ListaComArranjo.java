b03b2fcb6c4ce175a76a5e8a3f7da5fdcd6a6934
NatalicioNasciment <natalicio.eng.soft@gmail.com>
2016-09-03 17:00:54 -0300

    Alterando o metodo remover(ainda incompleto)

--- src/exercicio2/ListaComArranjo.java
+++ src/exercicio2/ListaComArranjo.java
@@ -20,30 +20,41 @@ public class ListaComArranjo {
 		}
 		return false;
 	}
-
-	private void aumentaCapacidade() {
-		if (this.ultimo == this.elementos.length) {
-			Object elementosAux[] = new Object[this.elementos.length * 2];
-			for (int i = 0; i < this.elementos.length; i++) {
-				elementosAux[i] = elementos[i];
+	public Object remover(int indice) throws Exception{
+		if(indice < 0 || indice > this.elementos.length){
+			throw new Exception("O indice informado não faz parte do intervalo que compõe a  lista");
+		}
+		
+		for(int i = 0; i < this.ultimo && !this.elementos[i].equals(indice); i++){
+			if(i >= this.ultimo){
+				return null;
 			}
-			this.elementos = elementosAux;
+			Object elemento ;
+			//livro pagina 84
+			
+		}
+		
+		return null;
+	}
+/*
+	public Object remover(int indice) throws Exception {
+		if (indice < 0 || indice > this.elementos.length) {
+			throw new Exception("O indice informado não existe na lista");
+		} else {
+			for (int i = indice; i < this.elementos.length - 1; i++) {
+				this.elementos[indice] = this.elementos[indice + 1];
+				
+				Object aux =  this.elementos[indice];
+				return aux;
+			}
+			
+			return -1;
 		}
 	}
+	*/
 
 	@Override
 	public String toString() {
-		/*StringBuilder s = new StringBuilder();
-		s.append("[");
-		for(int i = 0; i < this.ultimo-1; i++){
-			s.append(this.elementos[i]);
-			s.append(", ");
-		}
-		if(ultimo < 0){
-			s.append(this.elementos[ultimo-1]);
-		}
-		return elementos.toString();
-		*/
 		return " [elementos=" + Arrays.toString(elementos) + "]";
 	}
 
@@ -64,27 +75,31 @@ public class ListaComArranjo {
 		}
 		return -1;
 	}
-	
-	public Object buscarPorIntervalo(int indiceInicial, int indiceFinal) throws Exception{
-		Object resultadoIntervalo[] = null; 
-		int j =0;
-		if(indiceInicial < 0 || indiceFinal > this.elementos.length){
+
+	public Object buscarIntervalo(int indiceInicial, int indiceFinal) throws Exception {
+		Object resultadoIntervalo[] = null;
+		int j = 0;
+		if (indiceInicial < 0 || indiceFinal > this.elementos.length) {
 			throw new Exception("O intervalo informado não é válido");
-		}else{
-			for(int i = indiceInicial; i <= indiceFinal; i++, j++){
+		} else {
+			for (int i = indiceInicial; i <= indiceFinal; i++, j++) {
 				resultadoIntervalo[j] = elementos[i];
 			}
 		}
 		return resultadoIntervalo;
 	}
 
-	public void remover(int indice) throws Exception {
-		if (indice < 0 || indice > this.elementos.length) {
-			throw new Exception("O indice informado não existe na lista");
-		} else {
-			for (int i = indice; i < this.elementos.length - 1; i++) {
-				this.elementos[indice] = this.elementos[indice + 1];
+	public void alterar(int indice, Object elemento) {
+
+	}
+
+	private void aumentaCapacidade() {
+		if (this.ultimo == this.elementos.length) {
+			Object elementosAux[] = new Object[this.elementos.length * 2];
+			for (int i = 0; i < this.elementos.length; i++) {
+				elementosAux[i] = elementos[i];
 			}
+			this.elementos = elementosAux;
 		}
 	}
 }
--- src/exercicio2/TestArranjo.java
+++ src/exercicio2/TestArranjo.java
@@ -3,18 +3,24 @@ package exercicio2;
 public class TestArranjo {
 	public static void main(String[] args) throws Exception {
 		ListaComArranjo arranjo = new ListaComArranjo(2);
-			arranjo.adicionar("Natalicio");
 			arranjo.adicionar("Gerlane");
 			arranjo.adicionar("Assis");
 			arranjo.adicionar("Nataliano");
 			arranjo.adicionar(1);
 			arranjo.adicionar("Assis");
+			arranjo.adicionar("Daniele");
+			arranjo.adicionar("Natalicio");
+			
 			System.out.println(arranjo.toString());
-			arranjo.remover(0);
-			System.out.println(arranjo.toString());		
+			System.out.println();
+			
+			System.out.println("O elemento removido foi: "+ arranjo.remover(6));
+			System.out.println(arranjo.toString());	
+			
+			System.out.println();
 			System.out.println("Resultado da busca: "+ arranjo.bucarPorIndice(2));
 			System.out.println("O índice do elemento passado é : "+ arranjo.buscarPorElemento("Assis"));
-			System.out.println(arranjo.buscarPorIntervalo(-1, 3));
+//			System.out.println(arranjo.buscarPorIntervalo(-1, 3));
 	}
 
 }
