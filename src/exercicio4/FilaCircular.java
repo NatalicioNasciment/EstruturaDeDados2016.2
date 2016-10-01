001fb57f02235e1c7c073d24ed6b8176c972a3ea
NatalicioNasciment <natalicio.eng.soft@gmail.com>
2016-10-01 16:06:40 -0300

    Adicionando o metodo enfilerar

--- src/exercicio4/FilaCircular.java
+++ src/exercicio4/FilaCircular.java
@@ -7,28 +7,58 @@ public class FilaCircular<T> {
 	private int tam;
 
 	public FilaCircular(int tam) {
-		this.itens = (T[]) new Object[this.tam];
+		this.itens = (T[]) new Object[this.tam = tam];
 		this.frente = 0;
 		this.tras = 0;
 	}
 	
 	public void enfilerar(T item) throws Exception{
-		if(vazia()){
-			this.tras++;
-			this.itens[frente] = item;
-		}
-		if(((this.tras + 1) % tam) == this.frente){
+		if(cheia()){
 			throw new Exception("A lista está cheia!!!");
 		}
 		this.itens[tras] = item;
 		this.tras = (tras + 1) % tam;
 	}
 	
-	public boolean desenfilerar(){
-		return false;
+	public void desenfilerar() throws Exception{
+		if(vazia()){
+			throw new Exception("Fila vazia!!!");
+		}else{
+			T item =  itens[this.frente];
+			this.frente = (this.frente + 1) % tam;
+			this.itens[this.tras -1] = null;
+		}
 	}
+	
+	public void listar(){
+		for(int i = 0; i < this.tam; i++){
+			if(itens[i] != null){
+				System.out.println(itens[i]);
+			}
+		}
+	}
+	
+	public int quantElemento(){
+		return 0;
+	}
+	
+	private T redimensionar(){
+		T novosItens = (T)new Object[2 * tam];
+		if(this.tras + 1 ==  this.tam){
+			
+		}
+		return null;
+	}
+	
 	public boolean vazia(){
-		return frente == tras;
+		return this.frente == this.tras;
+	}
+	public boolean cheia(){
+		return (this.tras + 1) % tam == this.frente;
+	}
+	
+	public int getTam(){
+		return this.tam;
 	}
 
 }
--- src/exercicio4/TestFilaCircular.java
+++ src/exercicio4/TestFilaCircular.java
@@ -2,9 +2,19 @@ package exercicio4;
 
 public class TestFilaCircular {
 	public static void main(String[] args) throws Exception {
-		FilaCircular<Integer> fc = new FilaCircular<Integer>(2);
-		System.out.println(fc.vazia());
+		FilaCircular<Integer> fc = new FilaCircular<Integer>(5);
 		fc.enfilerar(3);
+		fc.enfilerar(3);
+		fc.enfilerar(3);
+		fc.enfilerar(3);
+		fc.listar();
+		System.out.println("----------------");
+		fc.desenfilerar();
+		fc.listar();
+//		System.out.println("Tamanho: " + fc.getTam());
+	
+	
+//		System.out.println("Lista esta vazia: " + fc.vazia());
 	}
 
 }
