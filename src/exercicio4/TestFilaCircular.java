2d30b76328deba7a1473a776ef0d7a8e06dd23e8
NatalicioNasciment <natalicio.eng.soft@gmail.com>
2016-10-02 11:38:12 -0300

    TestFilaCiruclar- Status -> ok

--- src/exercicio4/TestFilaCircular.java
+++ src/exercicio4/TestFilaCircular.java
@@ -2,19 +2,46 @@ package exercicio4;
 
 public class TestFilaCircular {
 	public static void main(String[] args) throws Exception {
-		FilaCircular<Integer> fc = new FilaCircular<Integer>(5);
-		fc.enfilerar(3);
-		fc.enfilerar(3);
-		fc.enfilerar(3);
+		FilaCircular<Integer> fc = new FilaCircular<Integer>(3);
+		fc.enfilerar(30);
+		fc.enfilerar(40);
+		fc.enfilerar(50);
+		fc.enfilerar(60);
+		fc.enfilerar(70);
+		fc.enfilerar(80);
+		fc.enfilerar(90);
+		fc.enfilerar(100);
 		fc.listar();
-		System.out.println("A quantidade de elemento: " + fc.quantElemento());
-		System.out.println("----------------");
 		fc.desenfilerar();
+		fc.desenfilerar();
+		fc.desenfilerar();
+		fc.desenfilerar();
+//		fc.desenfilerar();
+//		fc.desenfilerar();
+//		fc.desenfilerar();
+//		fc.desenfilerar();
+		System.out.println("------------------------------------------");
 		fc.listar();
-//		System.out.println("Tamanho: " + fc.getTam());
-	
-	
-//		System.out.println("Lista esta vazia: " + fc.vazia());
+		System.out.println("------------------------------------------");
+		System.out.println("A quantidade de elementos eh: " +fc.quantElemento());
+		System.out.println("------------------------------------------");
+		System.out.println("O tamanho da lista é : " + fc.getTam());
+		
+//		fc.listar();
+//		System.out.println();
+//		System.out.println(fc.desenfilerar());
+		// fc.listar();
+//		fc.listar();
+//		System.out.println();
+//		System.out.println(fc.desenfilerar());	
+		// System.out.println("A quantidade de elemento: " +
+		// fc.quantElemento());
+		// System.out.println("----------------");
+		// fc.desenfilerar();
+		// fc.listar();
+		// System.out.println("Tamanho: " + fc.getTam());
+
+		// System.out.println("Lista esta vazia: " + fc.vazia());
 	}
 
 }
