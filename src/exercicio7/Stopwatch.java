c33aebd7f2b66c4d164788ed6a6d667e48e31b2f
NatalicioNasciment <natalicio.eng.soft@gmail.com>
2016-10-13 20:50:50 -0300

    Exercicio 7 : Implementado => algoritmo threeSam com stopwatch

---
+++ src/exercicio7/Stopwatch.java
@@ -0,0 +1,16 @@
+package exercicio7;
+
+public class Stopwatch {
+
+	private final long start;
+
+	public Stopwatch() {
+		start = System.currentTimeMillis();
+	}
+
+	public double elapsedTime() {
+		long now = System.currentTimeMillis();
+		return (now - start) / 1000.0;
+	}
+
+}
---
+++ src/exercicio7/Test.java
@@ -0,0 +1,22 @@
+package exercicio7;
+
+import java.util.Random;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int n = 16000;
+		int x[] = new int[n];
+		Random ale = new Random();
+		for (int i = 0; i < n; i++) {
+			x[i] = ale.nextInt(5000) - 1000;
+		}
+
+		ThreeSum ts = new ThreeSum();
+		Stopwatch crono = new Stopwatch();
+		ts.count(n, x);
+		System.out.print(crono.elapsedTime());
+
+	}
+
+}
